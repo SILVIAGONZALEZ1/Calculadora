ae3aed3fa6f045992f369b8aaf3717a67548110c
Silvia Gonzalez <devsoftwaregonzalez@gmail.com>
2024-05-25 17:31:58 -0300

    Se suben archivos modificados Display.js e index.css

--- Display.js
+++ Display.js
@@ -1,12 +1,12 @@
 class Display {
     constructor(displayValorAnterior, displayValorActual) {
-        this.displayValorActual = displayValorActual;// Almacena el elemento del DOM para mostrar el valor actual
-        this.displayValorAnterior = displayValorAnterior;// Almacena el elemento del DOM para mostrar el valor anterior
-        this.calculador = new Calculadora();// Crea una instancia de la clase Calculadora
-        this.tipoOperacion = undefined;// Inicializa el tipo de operación como indefinido
-        this.valorActual = '';// Inicializa el valor actual como una cadena vacía
-        this.valorAnterior = '';// Inicializa el valor anterior como una cadena vacía
-        this.signos = {// Define los símbolos para las operaciones
+        this.displayValorActual = displayValorActual;
+        this.displayValorAnterior = displayValorAnterior;
+        this.calculador = new Calculadora();
+        this.tipoOperacion = undefined;
+        this.valorActual = '';
+        this.valorAnterior = '';
+        this.signos = {
             sumar: '+',
             dividir: '%',
             multiplicar: 'x',
@@ -15,41 +15,41 @@ class Display {
     }
 
     borrar() {
-        this.valorActual = this.valorActual.toString().slice(0,-1);// Elimina el último carácter del valor actual
-        this.imprimirValores();// Actualiza los valores mostrados en pantalla
+        this.valorActual = this.valorActual.toString().slice(0,-1);
+        this.imprimirValores();
     }
 
-    borrarTodo() { 
-        this.valorActual = ''; // Resetea el valor actual
-        this.valorAnterior = ''; // Resetea el valor anterior
-        this.tipoOperacion = undefined; // Resetea el tipo de operación
-        this.imprimirValores(); // Actualiza los valores mostrados en pantalla
+    borrarTodo() {
+        this.valorActual = '';
+        this.valorAnterior = '';
+        this.tipoOperacion = undefined;
+        this.imprimirValores();
     }
 
-    
-    computar(tipo) { 
-        this.tipoOperacion !== 'igual' && this.calcular(); // Si la operación no es 'igual', realiza el cálculo
-        this.tipoOperacion = tipo; // Establece el tipo de operación actual
-        this.valorAnterior = this.valorActual || this.valorAnterior; // Establece el valor anterior al valor actual o mantiene el valor anterior si el valor actual está vacío
-        this.valorActual = ''; // Resetea el valor actual
-        this.imprimirValores(); // Actualiza los valores mostrados en pantalla
-    }
-    agregarNumero(numero) { 
-        if(numero === '.' && this.valorActual.includes('.')) return // Evita agregar más de un punto decimal
-        this.valorActual = this.valorActual.toString() + numero.toString(); // Agrega el número al valor actual
-        this.imprimirValores(); // Actualiza los valores mostrados en pantalla
+    computar(tipo) {
+        this.tipoOperacion !== 'igual' && this.calcular();
+        this.tipoOperacion = tipo;
+        this.valorAnterior = this.valorActual || this.valorAnterior;
+        this.valorActual = '';
+        this.imprimirValores();
     }
 
-    imprimirValores() { 
-        this.displayValorActual.textContent = this.valorActual; // Muestra el valor actual en el display
-        this.displayValorAnterior.textContent = `${this.valorAnterior} ${this.signos[this.tipoOperacion] || ''}`; // Muestra el valor anterior y el tipo de operación en el display
+    agregarNumero(numero) {
+        if(numero === '.' && this.valorActual.includes('.')) return
+        this.valorActual = this.valorActual.toString() + numero.toString();
+        this.imprimirValores();
     }
 
-    calcular() { 
-        const valorAnterior = parseFloat(this.valorAnterior); // Convierte el valor anterior a un número
-        const valorActual = parseFloat(this.valorActual); // Convierte el valor actual a un número
+    imprimirValores() {
+        this.displayValorActual.textContent = this.valorActual;
+        this.displayValorAnterior.textContent = `${this.valorAnterior} ${this.signos[this.tipoOperacion] || ''}`;
+    }
 
-        if( isNaN(valorActual)  || isNaN(valorAnterior) ) return // Si alguno de los valores no es un número, termina la función
-        this.valorActual = this.calculador[this.tipoOperacion](valorAnterior, valorActual); // Realiza la operación usando el método correspondiente de la calculadora
+    calcular() {
+        const valorAnterior = parseFloat(this.valorAnterior);
+        const valorActual = parseFloat(this.valorActual);
+
+        if( isNaN(valorActual)  || isNaN(valorAnterior) ) return
+        this.valorActual = this.calculador[this.tipoOperacion](valorAnterior, valorActual);
     }
 }
